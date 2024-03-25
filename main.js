bd6a75b79c152a71846b717d12ae00d173c61097
sosarivero <81424281+sosarivero@users.noreply.github.com>
2024-03-25 10:09:56 +0000

    Imprimir tablero

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ for(let i=0; i < linea.length; i++) {
     tablero.push(linea[[i]].split(""))
 };
 console.log(tablero)
+
+function imprimirTablero(tablero) {
+    let tableroDiv = document.createElement("div");
+    tableroDiv.classList.add("tablero");
+   
+    for (let linea = 0; linea < tablero.length; linea++) {
+        let lineaDiv = document.createElement("div");
+        lineaDiv.classList.add("linea");
+        for (let celda = 0; celda < tablero[linea].length; celda++) {
+            console.log(tablero[linea])
+
+            let celdaDiv = document.createElement("div");
+            celdaDiv.innerText = tablero[linea][celda];
+            celdaDiv.classList.add("celda");
+            lineaDiv.appendChild(celdaDiv)
+        }
+
+        tableroDiv.appendChild(lineaDiv);
+    }
+    document.body.appendChild(tableroDiv);
+}
+
+imprimirTablero(tablero);
