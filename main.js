88e6dd17c497dbd8256375da915fe019f67d5e54
sosarivero <81424281+sosarivero@users.noreply.github.com>
2024-03-25 10:31:50 +0000

    Añadir comentarios

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 // construir el tablero
 
-let nivel = "#######|#-----#|#-JC.-#|#-----#|#######"
-let linea = nivel.split("|")
-let tablero = []
-for(let i=0; i < linea.length; i++) {
-    tablero.push(linea[[i]].split(""))
-};
-console.log(tablero)
+let nivel = "#######|#-----#|#-JC.-#|#-----#|#######";
+let linea = nivel.split("|");
+let tablero = [];
+for (let i = 0; i < linea.length; i++) {
+  tablero.push(linea[[i]].split(""));
+}
+console.log(tablero);
 
 function imprimirTablero(tablero) {
-    let tableroDiv = document.createElement("div");
-    tableroDiv.classList.add("tablero");
-   
-    for (let linea = 0; linea < tablero.length; linea++) {
-        let lineaDiv = document.createElement("div");
-        lineaDiv.classList.add("linea");
-        for (let celda = 0; celda < tablero[linea].length; celda++) {
-            console.log(tablero[linea])
+  // Crea un div que representa el tablero entero
+  let tableroDiv = document.createElement("div");
+  tableroDiv.classList.add("tablero");
 
-            let celdaDiv = document.createElement("div");
-            celdaDiv.innerText = tablero[linea][celda];
-            celdaDiv.classList.add("celda");
-            lineaDiv.appendChild(celdaDiv)
-        }
+  // Bucle para crear un div para cada línea del tablero
+  for (let linea = 0; linea < tablero.length; linea++) {
+    let lineaDiv = document.createElement("div");
+    lineaDiv.classList.add("linea");
+    // Bucle para crear divs para cada celda de cada línea
+    for (let celda = 0; celda < tablero[linea].length; celda++) {
+      console.log(tablero[linea]);
 
-        tableroDiv.appendChild(lineaDiv);
+      let celdaDiv = document.createElement("div");
+      celdaDiv.innerText = tablero[linea][celda];
+      celdaDiv.classList.add("celda");
+      // Añade cada celda a la línea actual
+      lineaDiv.appendChild(celdaDiv);
     }
-    document.body.appendChild(tableroDiv);
+    // Añade línea al tablero
+    tableroDiv.appendChild(lineaDiv);
+  }
+  // Añade el tablero al body de HTML, para por fin hacer visible todos los elementos creados
+  document.body.appendChild(tableroDiv);
 }
 
 imprimirTablero(tablero);
