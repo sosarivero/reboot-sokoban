ddb89d40829d3842f58026069f9b5b99952228bb
Madalenaxpto <madalenaposser@hotmail.com>
2024-03-25 09:35:15 +0000

    tablero done

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hello")
+// construir el tablero
+
+let nivel = "#####|#@$.#|#####"
+let linea = nivel.split("|")
+let tablero = []
+for(let i=0; i < linea.length; i++) {
+    tablero.push(linea[[i]].split(""))
+};
+console.log(tablero)
